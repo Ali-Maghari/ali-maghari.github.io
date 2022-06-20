701367433ba5a93461dc35d1d6cf6693c439e3d6
Ali-Maghari <aliplaywork@gmail.com>
2022-06-20 12:38:24 +0300

    Some fixes

--- js/secondary.js
+++ js/secondary.js
@@ -1,4 +1,4 @@
-var map = {
+var appsMap = {
     "to_do_list": {
         "title": "To Do List",
         "description": "It is an application that helps users to do their tasks, and also helps them to schedule it so that this application provides a notification when the task that the user entered is due, and the user can also use it as a notes application so that he only enters his task without specifying a time and date for it.",
@@ -14,7 +14,22 @@ var map = {
             "img/to_do_list/images/9.png",
         ],
     },
-}
+};
+
+var arrWorkingExperience = [
+    {
+        "job_title": "Freelance Work",
+        "company_name": "Freelancer and Mostaql",
+        "job_description": "Work on freelance platforms such as Mostaql and Freelancer with many clients .",
+        "start_to_end_date": "2019 - 2022",
+    },
+    {
+        "job_title": "Android Developer",
+        "company_name": "One To Three Company",
+        "job_description": "Work as a developer of Android applications in Java and Kotlin languages . ",
+        "start_to_end_date": "2022 - until now",
+    }
+]
 
 function setAppId(id) {
     window.localStorage.setItem("app_id", id);
@@ -25,22 +40,22 @@ function setAppId(id) {
 function setAppTitle() {
     var app_id = window.localStorage.getItem("app_id");
     for (var i = 0; i < document.getElementsByClassName("app_title").length; i++) {
-        document.getElementsByClassName("app_title")[i].innerHTML = map[app_id].title;
+        document.getElementsByClassName("app_title")[i].innerHTML = appsMap[app_id].title;
     }
 }
 
 function setAppDescription() {
     var app_id = window.localStorage.getItem("app_id");
     for (var i = 0; i < document.getElementsByClassName("app_description").length; i++) {
-        document.getElementsByClassName("app_description")[i].innerHTML = map[app_id].description;
+        document.getElementsByClassName("app_description")[i].innerHTML = appsMap[app_id].description;
     }
 }
 
 function getAppImages() {
     var app_id = window.localStorage.getItem("app_id");
-    var images = map[app_id].images;
-    for (var i = 0; i < map[app_id].images.length; i++) {
-        images[i] = map[app_id][i];
+    var images = appsMap[app_id].images;
+    for (var i = 0; i < appsMap[app_id].images.length; i++) {
+        images[i] = appsMap[app_id][i];
     }
     return images;
 }
@@ -49,7 +64,7 @@ function setAppImagesPaginationIndicator() {
     var limit = 6;
     var app_id = window.localStorage.getItem("app_id");
     var page = parseInt(localStorage.getItem("page"));
-    var images_number = map[app_id].images.length;
+    var images_number = appsMap[app_id].images.length;
     var current_page = page;
     page = Math.ceil(images_number / limit);
     document.write("<div class='my-4'><ul class='pagination justify-content-center'>");
@@ -78,7 +93,7 @@ function setAppImages() {
     var limit = 6;
     var app_id = window.localStorage.getItem("app_id");
     var page = parseInt(localStorage.getItem("page"));
-    var images_number = map[app_id].images.length;
+    var images_number = appsMap[app_id].images.length;
     var current_page = page;
     var start = (current_page - 1) * limit;
     var end = current_page * limit;
@@ -98,8 +113,8 @@ function setAppImages() {
             "                        <div class='col-12'>\n" +
             "                            <div class='numbers'>\n" +
             "                                <h5 class='text-sm font-weight-bolder mb-0'>\n" +
-            "                                    <a href='" + map[app_id].images[i] + "'>\n" +
-            "                                        <img src='" + map[app_id].images[i] + "' alt='app screen image' class='border-radius-lg bg-cover' style='height: 100%; width: 100%; object-fit: cover'>\n" +
+            "                                    <a href='" + appsMap[app_id].images[i] + "'>\n" +
+            "                                        <img src='" + appsMap[app_id].images[i] + "' alt='app screen image' class='border-radius-lg bg-cover' style='height: 100%; width: 100%; object-fit: cover'>\n" +
             "                                    </a>\n" +
             "                                </h5>\n" +
             "                            </div>\n" +
@@ -113,7 +128,7 @@ function setAppImages() {
 
 function showNoAppImagesFoundedMessage() {
     var app_id = window.localStorage.getItem("app_id");
-    var images_number = map[app_id].images.length;
+    var images_number = appsMap[app_id].images.length;
     document.write("<div class='mb-5 mt-4'><div class='align-middle text-center'><span class='text-secondary text-xs font-weight-bold'>Sorry, there is no data to display</span></div></div>");
     if (images_number > 0) {
         document.write("<div class='centered-button justify-content-center py-0 px-4'>\n" +
@@ -143,6 +158,30 @@ function onGoBackButtonClicked(page) {
 
 function goBackToPreviousPage() {
     history.back();
+}
+
+function setWorkingExperience() {
+    for (var i = 0; i < arrWorkingExperience.length; i++) {
+        var slideStyle = "slideInLeft";
+        var direction = "left";
+        if (i % 2 === 0) {
+            slideStyle = "slideInLeft";
+            direction = "left";
+        }
+        else {
+            slideStyle = "slideInRight";
+            console.log("reached here");
+            direction = "right";
+        }
+        document.write("<div class='timeline-item " + direction + " wow " + slideStyle + "' data-wow-delay='0.1s'>\n" +
+            "                        <div class='timeline-text'>\n" +
+            "                            <div class='timeline-date'>" + arrWorkingExperience[i].start_to_end_date + "</div>\n" +
+            "                            <h2>" + arrWorkingExperience[i].job_title + "</h2>\n" +
+            "                            <h4>" + arrWorkingExperience[i].company_name + "</h4>\n" +
+            "                            <p>" + arrWorkingExperience[i].job_description + "</p>\n" +
+            "                        </div>\n" +
+            "                    </div>");
+    }
 }
 
 
